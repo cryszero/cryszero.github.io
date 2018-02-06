e04c2192d7daebf33e0571aee2678960166b0609
Alexey Pervov <elfpirate@gmail.com>
2018-02-06 08:11:16 +0500

    New design start

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
-var button = document.querySelector('.about-button');
-var modal = document.querySelector('.aboutme');
+$('.page-navbar__button').click(function() {
+	$('.page-navbar__list').toggleClass('page-navbar__list--visible');
+});
 
-button.addEventListener('click', function(evt){
-	evt.preventDefault();
-	modal.classList.toggle('modal_visible');
+$(function() {
+var stickyHeader = $('.page-navbar').offset().top;
+
+$(window).scroll(function() {
+	if($(window).scrollTop() >= stickyHeader) {
+		$('.page-navbar').addClass('page-navbar--sticky');
+	}
+	else {
+		$('.page-navbar').removeClass('page-navbar--sticky');
+	}
+});
 });
