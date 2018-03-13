d48cc352ab1a0a6fe29113d4e0c8b66644b3ef25
Alexey Pervov <elfpirate@gmail.com>
2018-03-13 07:03:37 +0500

    Kotya

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,31 @@ $(document).ready(() => {
         $('.page-navbar__button').toggleClass('page-navbar__button--active');
     });
 
+    // $('#kitten').click((evt) => {
+    //     evt.preventDefault();
+    //     var font_size = Math.random() * 20 + 10;
+    //     ReactDOM.render(React.createElement(
+    //         'div', { className: 'meow-modal', style: { fontSize: font_size } },
+    //         'Meow'
+    //     ), document.getElementById('about'));
+    // }); побаловался с реактом, но это не совсем то, что нужно)
 
+    $('#kitten').click((evt) => {
+        evt.preventDefault();
+        var aboutStart = $('#about').offset().top;
+        var aboutEnd = aboutStart + $('#about').height();
+        posX = (Math.random() * ($('#about').width() - 103) + 103).toFixed();
+        posY = (Math.random() * (aboutEnd - aboutStart) + aboutStart).toFixed();
+        $meow = $('<div/>').text('Meow').css({
+            'position': 'absolute',
+            'fontSize': Math.floor(Math.random() * 30 + 20) + 'px',
+            'left': posX+'px',
+            'top': posY+'px',
+            'z-index': '-1'
+        }).appendTo('#about').fadeIn(1000).delay(1000).fadeOut(500, function() {
+            $(this).remove();
+        });
+    });
 
     $('.page-navbar__link').click(function() { // Гладкий скролл до якоря
         var href = $(this).attr('href');
@@ -31,23 +55,23 @@ $(document).ready(() => {
     });
 
     $('.contacts__form').submit(function() {
-    	var form = $(this);
-    	var data = form.serialize();
-    	$.ajax({
-    		type: 'POST',
-    		url: 'send.php',
-    		dataType: 'json',
-    		data: data,
-    		beforeSend: function(data) {
-    			form.find('input[type="submit"]').attr('disabled', 'disabled');
-    		},
-    		success: function(data) {
-    			alert('Пысьмо отправлено');
-    		},
-    		complete: function(data) {
-    			form.find('input[type="submit"]').prop('disabled', false);
-    		}
-    	});
-    	return false;
+        var form = $(this);
+        var data = form.serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'send.php',
+            dataType: 'json',
+            data: data,
+            beforeSend: function(data) {
+                form.find('input[type="submit"]').attr('disabled', 'disabled');
+            },
+            success: function(data) {
+                alert('Пысьмо отправлено');
+            },
+            complete: function(data) {
+                form.find('input[type="submit"]').prop('disabled', false);
+            }
+        });
+        return false;
     });
 });
